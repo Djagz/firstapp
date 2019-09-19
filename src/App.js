1815bade484f5877fb0eb8495cd976d2135ef98d
Djagz <jagwanidipesh85@gmail.com>
2019-09-18 20:54:27 -0700

    Base Code

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Dipesh = (props) => {
+  return(
+      <div>
+        Name: <input type="text" />
+        <br />
+        <button>Add</button>
+      </div>
+    )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Dipesh />
       </header>
     </div>
   );
