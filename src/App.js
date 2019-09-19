ca3e5ecad934317b5743bbd70bef776a0fc0eb49
Djagz <jagwanidipesh85@gmail.com>
2019-09-18 21:09:32 -0700

    Saving on branch testuser

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Test from './test';
 
 const Dipesh = (props) => {
   return(
@@ -17,6 +18,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        <Dipesh />
+       <Test />
       </header>
     </div>
   );
---
+++ src/test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Test extends React.Component{
+	constructor(){
+		super()
+
+	}
+
+	render(){
+		return(
+			<div>Test User</div>
+			)
+	}
+}
+
+export default Test
